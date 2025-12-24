6a4d99e690847aff50e405db95720f0c14c5d642
Aldi-H <aldihimawan88@gmail.com>
2025-12-24 12:41:52 +0700

    Update folder path and output filename for December Excel merging

--- excel-omspan-merge.py
+++ excel-omspan-merge.py
@@ -3,7 +3,7 @@ import glob
 import os
 
 # folder_path = "/mnt/d/Work/Job/CPNS/Perbendaharaan/OMSPAN/2025/DAK_TPG_NOVEMBER"
-folder_path = "/mnt/d/Work/Job/CPNS/Perbendaharaan/OMSPAN/2025/DAK_TAMBAHAN_PENGHASILAN_NOVEMBER"
+folder_path = "/mnt/d/Work/Job/CPNS/Perbendaharaan/OMSPAN/2025/DESEMBER/DAK_TPG_DESEMBER/KESELURUHAN"
 
 excel_files = glob.glob(os.path.join(folder_path, "*.xlsx")) + \
             glob.glob(os.path.join(folder_path, "*.xls"))
@@ -23,11 +23,16 @@ else:
                 "NIP": str,
                 "NO. REKENING": str,
             })
+        
+        df.insert(0, "SOURCE FILE", os.path.basename(file))
+
         dfs.append(df)
     
     merged_df = pd.concat(dfs, ignore_index=True)
     
-    output_path = os.path.join(folder_path, "DAK_TAMBAHAN_PENGHASILAN_NOVEMBER.xlsx")
+    merged_df.insert(1, "ANGKATAN", merged_df["NIP"].str[8:12])
+    
+    output_path = os.path.join(folder_path, "DAK_TPG_DESEMBER.xlsx")
     merged_df.to_excel(output_path, index=False)
     
     print(f"\nMerged {len(dfs)} files successfully!")
